213072f305604ba4cb4650f7d61c06f53842fa21
Nikhil <nikhil.4002.50.82@gmail.com>
2026-08-21 18:12:26 +0530

    Update eslint.config.mjs

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -13,6 +13,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/preserve-manual-memoization": "off",
+      "react-hooks/refs": "off"
+    },
+  }
 ]);
 
 export default eslintConfig;
